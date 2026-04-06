98f83dc56c67cc8597237dc7c154ce0c0f5fe1c4
AlminazaDennis <dennisitlab.com>
2026-04-06 08:46:57 +0800

    app use cors added

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const { connectToMongoDB } = require("./database");
 const router = require("./routes");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
